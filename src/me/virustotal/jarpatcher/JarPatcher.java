effa887d05cd7b49bb4c2189c36047e860a55501
virustotalop <virustotalop@gmail.com>
2015-11-04 21:01:08 -0800

    Code cleanup
    Cleaned up the code a bit, removed CustomLoader.java as it wasn't
    needed.

--- src/me/virustotal/jarpatcher/JarPatcher.java
+++ src/me/virustotal/jarpatcher/JarPatcher.java
@@ -1,15 +1,13 @@
 package me.virustotal.jarpatcher;
 
 import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Enumeration;
-import java.util.Stack;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
@@ -19,21 +17,28 @@ public class JarPatcher {
 	private static File classFolder = new File("classes");
 	private static ArrayList<String> loadedClasses = new ArrayList<String>();
 	private static URLClassLoader urlLoader;
-	
+	private static boolean debug = false;
+
 	public static void main(String[] args)
 	{
-		
-		
-			
-
 		if(!classFolder.exists())
 			classFolder.mkdir();
-		
+
 		String[] newArgs = null;
+		
+		if(args.length == 0)
+		{
+			System.out.println("A jar file must be provided!");
+			return;
+		}
+		
 		if(args.length > 1)
 		{
-			System.out.println(args[1]);
 			newArgs = args[1].split(" ");
+			if(Arrays.asList(newArgs).contains("--debug"))
+			{
+				JarPatcher.debug = true;
+			}
 		}
 
 		File file = new File(args[0]);
@@ -46,32 +51,26 @@ public class JarPatcher {
 		try 
 		{
 			urlLoader = new URLClassLoader(new URL[] {classFolder.toURL(), file.toURL()}, JarPatcher.class.getClassLoader());
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
+		} 
+		catch (MalformedURLException e) 
+		{
 			e.printStackTrace();
 		}
-		
-		
+
 		if(classFolder.listFiles().length > 0)
 		{
 			JarPatcher.loadClassFiles(classFolder.listFiles());
 		}
-		
-		
-		
+
 		JarFile jarFile = null;
 		Manifest manifest = null;
 		String main = null;
 		Enumeration<JarEntry> jarEntries = null;
-		
 		try
 		{
 			jarFile = new JarFile(file);
 			manifest = jarFile.getManifest();
 			main = manifest.getMainAttributes().getValue("Main-Class");
-			//JarPatcher.setURI(file);
-			//urlLoader = new URLClassLoader(new URL[] {file.toURL()}, JarPatcher.class.getClassLoader());
-			System.out.println("url: " + file.toURL().getPath());
 			jarEntries = jarFile.entries();
 		}
 		catch(Exception ex)
@@ -82,7 +81,6 @@ public class JarPatcher {
 		while(jarEntries.hasMoreElements())
 		{
 			JarEntry entry = jarEntries.nextElement();
-			
 
 			if(entry.getName().endsWith(".class"))
 			{
@@ -90,11 +88,15 @@ public class JarPatcher {
 				entryName = entryName.substring(0, entryName.lastIndexOf("."));
 				try 
 				{
-					System.out.println(entryName);
 					if(!JarPatcher.loadedClasses.contains(entryName))
+					{
 						urlLoader.loadClass(entryName);
-					else
-						System.out.println("Contains entry name!");
+						
+						if(debug)
+						{
+							System.out.println(entryName);
+						}
+					}
 				}
 				catch(NoClassDefFoundError | ClassNotFoundException ex) //This should be updated in the future, I blame maven for making me add this
 				{
@@ -107,11 +109,17 @@ public class JarPatcher {
 			Class<?> mainClass = urlLoader.loadClass(main);
 			Method mainMethod = mainClass.getDeclaredMethod("main", new Class[] {String[].class});
 
-			if(newArgs != null)
+			if(JarPatcher.debug)
 			{
-				for(String string : newArgs)
+				if(newArgs != null)
 				{
-					System.out.println("args: " + string);
+					String str = "args: ";
+					for(String string : newArgs)
+					{
+						str += string + " ";
+					}
+					
+					System.out.println(str);
 				}
 			}
 
@@ -124,7 +132,7 @@ public class JarPatcher {
 			ex.printStackTrace();
 		}
 	}
-	
+
 	private static void loadClassFiles(File[] files)
 	{
 		try
@@ -133,22 +141,20 @@ public class JarPatcher {
 			{
 				if(file.isFile())
 				{
-					System.out.println("url: " + new File("classes").toURL().getPath());
 					String absPath = file.getAbsolutePath();
 					String classesPath = absPath.substring(absPath.indexOf("classes"));
 					String entryName = classesPath.substring(classesPath.indexOf("\\") + 1).replace("\\", ".");
 					entryName = entryName.substring(0, entryName.indexOf(".class"));
-					System.out.println("entname: " + entryName);
 					urlLoader.loadClass(entryName);
 					loadedClasses.add(entryName);
-					System.out.println("Loaded file!");
 				}
 				else 
 				{
 					JarPatcher.loadClassFiles(file.listFiles());
 				}
 			}
-		}catch(StackOverflowError | NullPointerException | ClassNotFoundException | IOException | SecurityException | IllegalArgumentException e)
+		}
+		catch(StackOverflowError | NullPointerException | ClassNotFoundException | SecurityException | IllegalArgumentException e)
 		{
 			e.printStackTrace();
 		}
